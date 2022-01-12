c0790b4c2f04481b3f8687c2d1ad510a8b291a3f
Salman Shuaib <salman.shuaib@gmail.com>
2022-01-11 21:49:24 -0500

    working sample ETH contract

--- w-a-r.sol
+++ w-a-r.sol
@@ -1,17 +1,20 @@
-pragma solidity ^0.8.11;
+/// SPDX-License-Identifier: CC0-1.0
+/// http://remix.ethereum.org/#optimize=false&runs=200&evmVersion=null&version=soljson-v0.8.7+commit.e28d00a7.js
+
+pragma solidity ^0.8.7;
 
 contract MyContract {
     string value;
 
-    constructor() public {
+    constructor() {
         value = "myValue";
     }
 
-    function get() public view returns (string) {
+    function get() public view returns (string memory) {
         return value;
     }
 
-    function set(string _value) public {
+    function set(string memory _value) public {
         value = _value;
     }
 }
