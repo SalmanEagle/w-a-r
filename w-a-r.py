e5878bcd6bc7f07075e097841402d589dc1d2944
Salman Shuaib <salman.shuaib@gmail.com>
2022-02-02 21:24:53 -0500

    going on a tangent, switching from Reddit API to Virtual WAR Tokens as currency in forum

--- w-a-r.py
+++ w-a-r.py
@@ -1,33 +1,4 @@
-import requests
-
-CLIENT_ID = 'qonSPuxR3d3sTL7XEOr7IQ'
-SECRET_KEY = 'BazBl3t-8GAnOwux_M7fWNb4OMlK9g'
-
-auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
-
-with open('pw.txt', 'r') as f:
-    pw = f.read()
-
-data = {
-    'grant_type': 'password',
-    'username': 'SalmanEagle',
-    'password': pw
-}
-
-headers = {'User-Agent': 'w-a-r_API/0.0.1'}
-
-res = requests.post('https://www.reddit.com/api/v1/access_token',
-                    auth=auth, data=data, headers=headers)
-
-print(res.json())
-
-# print(res.json())
-''' TOKEN = res.json()['access_token']
-
-headers = {**headers, **{'Authorization': f'bearer {TOKEN}'}}
-
-res = requests.get(
-    'https://oauth.reddit.com/r/FlowerEconomics/hot', headers=headers)
-
-res.json()
+''' 
+Writing code to RATE Job Attempts (Completions) at FlowerEconomics.com forum (Python or PHP), via virtual WAR Tokens [vWARs] ~ backed by 
+actual WAR Tokens [aWARs] on the ETH blockchain.
 '''
