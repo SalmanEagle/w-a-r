58545389d53cc73be191cc8dcf60ebd3e8f7f2b1
Salman Shuaib <salman.shuaib@gmail.com>
2022-02-03 05:00:29 -0500

    Python REST API to post to WordPress website

--- w-a-r.py
+++ w-a-r.py
@@ -1,4 +1,12 @@
 ''' 
-Writing code to RATE Job Attempts (Completions) at FlowerEconomics.com forum (Python or PHP), via virtual WAR Tokens [vWARs] ~ backed by 
+- Writing code to RATE Job Attempts (Completions) at FlowerEconomics.com forum (Python or PHP), via virtual WAR Tokens [vWARs] ~ backed by 
 actual WAR Tokens [aWARs] on the ETH blockchain.
+- Visitors shoudl be able to post (articles), and have them voted on.
+'''
+
+
+'''
+References:
++ https://www.youtube.com/watch?v=AS6EPzDyjhA
++ https://hevodata.com/learn/wordpress-python/
 '''
