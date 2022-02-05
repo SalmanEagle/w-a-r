84077c7b64ca111f913df94d24d4eb31df6af2f4
Salman Shuaib <salman.shuaib@gmail.com>
2022-02-04 21:45:27 -0500

    establishing api.py with https://www.youtube.com/watch?v=AS6EPzDyjhA

---
+++ api.py
@@ -0,0 +1,5 @@
+import requests
+import json
+import base64
+
+url = 'https://flowereconomics.com/wp-json/wp/v2'
--- w-a-r.py
+++ w-a-r.py
@@ -1,12 +1,14 @@
 ''' 
-- Writing code to RATE Job Attempts (Completions) at FlowerEconomics.com forum (Python or PHP), via virtual WAR Tokens [vWARs] ~ backed by 
+- Writing code to RATE Job Attempts (or Completions) at FlowerEconomics.com forum (Python or PHP), via virtual WAR Tokens [vWARs] ~ backed by 
 actual WAR Tokens [aWARs] on the ETH blockchain.
-- Visitors shoudl be able to post (articles), and have them voted on.
+- Visitors should be able to post (articles), and have them voted on.
++ "Application Passwords" plugin set up at FE website.
 '''
 
 
 '''
 References:
++ https://discussion.dreamhost.com/t/how-to-post-content-to-wordpress-using-python-and-rest-api/65166
 + https://www.youtube.com/watch?v=AS6EPzDyjhA
 + https://hevodata.com/learn/wordpress-python/
 '''
