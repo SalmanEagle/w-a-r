29ad8b621d44a6e69db0f02826de4052cdea8d55
Salman Shuaib <salman.shuaib@gmail.com>
2022-02-06 21:24:24 -0500

    Wordpress REST API with PYTHON: https://www.youtube.com/watch?v=AS6EPzDyjhA

--- api.py
+++ api.py
@@ -6,3 +6,20 @@ url = 'https://flowereconomics.com/wp-json/wp/v2'
 
 user = 'salmanshuaib'
 password = 'A5xz bDWB 9rRP FvxQ 5RpW XWPw'
+
+creds = user + ':' + password
+
+token = base64.b64encode(creds.encode())
+
+header = {'Authorization': 'Basic ' + token.decode('utf-8')}
+
+post = {
+    'date': '2019-09-21T10:00:00',
+    'title': 'First API Post',
+    'content': 'This is our first pythond wordpress api post!',
+    'status': 'publish'
+
+}
+
+r = requests.post(url + '/posts', headers=header, json=post)
+print(r)
